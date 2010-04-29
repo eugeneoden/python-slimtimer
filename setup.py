e9a8f5a515dc38dde2826228271ce741d70f15f4
Eugene Oden <eugeneoden@gmail.com>
2010-04-28 23:46:41 -0500

    preparing to publish

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+PACKAGE = 'python-slimtimer'
+VERSION = '0.1.0'
+
+setup(
+    name=PACKAGE,
+    version=VERSION,
+
+    packages=['slimtimer'],
+    install_requires = ['elementtree>=1.2.6-20050316'],
+    description='SlimTimer interface for Python',
+    keywords='slimtimer slim timer',
+    license = 'BSD',
+    url = 'http://pypi.python.org/pypi/python-slimtimer',
+    author='Eugene Oden',
+    author_email='eugeneoden@gmail.com',
+    long_description="""
+    Provides an interface for SlimTimer.com, a time-tracking website.
+    """,
+    zip_safe=True
+)
+
